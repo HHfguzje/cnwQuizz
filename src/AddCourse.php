d1fc520239003afb55ebc781aa1ad9a802517477
LeonCromwell <mailyhai814@gmail.com>
2023-12-13 21:23:39 +0700

    fix bug addcourses

--- src/AddCourse.php
+++ src/AddCourse.php
@@ -2,6 +2,7 @@
 include '../function.php';
 session_start();
 $currentUser = $_SESSION['currentUser'];
+$listCourses = getAllCourses();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -45,16 +46,31 @@ $currentUser = $_SESSION['currentUser'];
     </main>
 
     <?php
+
     if (isset($_POST['btn'])) {
         $courseName = $_POST['course_name'];
-        if (!empty($courseName)) {
-            $result = createCourse($courseName);
-            if ($result) {
-                echo "<div class='alert alert-success text-center' role='alert'>Thêm khóa học thành công</div>";
+        $check = false;
 
-            } else {
-                echo "<div class='alert alert-warning text-center' role='alert'>Thêm khóa học thất bại" . mysqli_error($conn) . "</div>";
+        if (!empty($courseName)) {
+            foreach ($listCourses as $course) {
+                if ($course['course'] == $courseName) {
+                    $check = true;
+                    echo "<div class='alert alert-danger text-center' role='alert'>Khóa học đã tồn tại</div>";
+                    exit();
+                }
             }
+            if ($check == false) {
+                $result = createCourse($courseName);
+                if ($result) {
+                    echo "<script>alert('Thêm khóa học thành công')
+                        window.location.href = 'CourseManagement.php';
+                    </script>";
+
+                } else {
+                    echo "<div class='alert alert-warning text-center' role='alert'>Thêm khóa học thất bại" . mysqli_error($conn) . "</div>";
+                }
+            }
+
         } else {
             echo "<div class='alert alert-success text-center' role='alert'>Vui lòng nhập đủ thông tin</div>";
         }
