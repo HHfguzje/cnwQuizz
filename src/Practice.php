1e3fe0f838deec94f9c1fda9262254133f7e0602
Huy Hoang <hoang03.it@gmail.com>
2023-12-19 16:16:30 +0700

    bug loi

--- src/Practice.php
+++ src/Practice.php
@@ -23,11 +23,6 @@ function checkType($type)
 if (isset($_POST['countdown_expired'])) {
     header("location: Point.php?course_id=$course_id");
 }
-
-$check = isUserEnrolled($currentUser['id'], $course_id);
-if (!$check) {
-    header("location: courses.php");
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
